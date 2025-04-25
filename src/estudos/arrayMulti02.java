28bf79107675eb9024ef5b5cb40547b78190e5c1
munofoente <munofoente@gmail.com>
2025-04-25 14:33:15 -0300

    novos exercícios

--- src/estudos/arrayMulti02.java
+++ src/estudos/arrayMulti02.java
@@ -2,16 +2,29 @@ package estudos;
 
 public class arrayMulti02 {
     public static void main(String[] args) {
+        //explorando diferentes tipos de inicialização de arrays multidimensionais
         int[][] array = new int[4][];
+        int[] array3 = {1, 2, 3};
 
         array[0] = new int[1];
         array[1] = new int[2];
-        array[2] = new int[3];
-        array[3] = new int[4];
+        array[2] = array3;
+        array[3] = new int[] {1,2,3,4};
+
+        int[][] outroArray = {{0,1},{0,1,2},{0,1,2,3}};
 
         for (int[] arrayBase: array){
             for (int num : arrayBase){
                 System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("----------------");
+
+        for (int[] outroArrayBase : outroArray){
+            for (int num : outroArrayBase){
+                System.out.print(num);
             }
             System.out.println();
         }
