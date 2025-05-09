c16b18a23221be47fd815ace9f0b5ebb44bdf841
munofoente <munofoente@gmail.com>
2025-05-09 11:07:24 -0300

    Exercício pensionato

--- src/estudos/Pensionato/application/Program.java
+++ src/estudos/Pensionato/application/Program.java
@@ -1,4 +1,42 @@
 package estudos.Pensionato.application;
+import estudos.Pensionato.entities.Pensionato;
+
+import java.util.Scanner;
 
 public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Pensionato[] vect = new Pensionato[10];
+        int rooms;
+
+        System.out.print("How many rooms will be rented? ");
+        rooms = sc.nextInt();
+        int[] rents = new int[rooms];
+
+        for (int i = 0; i < rents.length; i++) {
+            System.out.println();
+            System.out.println("Rent #" + (i+1));
+            System.out.print("Name: ");
+            String name = sc.next();
+            sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.next();
+            sc.nextLine();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+
+            vect[room] = new Pensionato(name, email);
+        }
+
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+                if (vect[i] != null){
+                    System.out.println("Busy rooms: " + i + ", " + vect[i]);
+                }
+        }
+
+
+        sc.close();
+
+    }
 }
--- src/estudos/Pensionato/entities/Pensionato.java
+++ src/estudos/Pensionato/entities/Pensionato.java
@@ -1,4 +1,33 @@
 package estudos.Pensionato.entities;
 
 public class Pensionato {
+    private String name, email;
+
+    public Pensionato(){
+
+    }
+
+    public Pensionato(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+
+    public String toString(){
+        return name + ", " + email;
+    }
+
 }
