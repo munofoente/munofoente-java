c72d3962c67d6638571beef41771eb1c521020a5
Murilo Luiz <munofoente@gmail.com>
2025-08-26 11:01:22 -0300

    Exercício sobre polimorfismo

--- src/estudos/polimorfismo/application/Program.java
+++ src/estudos/polimorfismo/application/Program.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Object> employeeList = new ArrayList<>();
+        List<Employee> employeeList = new ArrayList<>();
 
         System.out.println("Enter the number of employees: ");
         int n = sc.nextInt();
