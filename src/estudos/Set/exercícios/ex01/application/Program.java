e822618f93c598810ff61468eaceec9269cccda3
Murilo Luiz <munofoente@gmail.com>
2026-01-13 22:58:21 -0300

    Exercícios com Set

---
+++ src/estudos/Set/exercícios/ex01/application/Program.java
@@ -0,0 +1,46 @@
+package estudos.Set.exercícios.ex01.application;
+
+import estudos.Set.exercícios.ex01.entities.LogEntry;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Date;
+
+public class Program {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the full file path: ");
+        String path = sc.next();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+
+            Set<LogEntry> set = new HashSet<>();
+
+            String line = br.readLine();
+            while(line != null){
+
+                String[] fields = line.split(" ");
+                String username = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+
+                set.add(new LogEntry(username, moment));
+
+                line = br.readLine();
+            }
+
+            System.out.println("Total users: " + set.size());
+
+        } catch(IOException e){
+            System.out.println("File not found" + e.getMessage());
+        }
+
+
+        sc.close();
+    }
+}
